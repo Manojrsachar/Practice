8d00d30f194f03696e160f775f33fa9d7499acfd
Manojrsachar <141207944+Manojrsachar@users.noreply.github.com>
2023-08-21 22:59:57 +0530

    Practice questions

---
+++ Labelloops/src/doapp.java
@@ -0,0 +1,26 @@
+public class doapp{
+
+public static void main(String[] args) {
+
+	int n=1;
+	
+manu:do
+	{ 
+		int m=1;
+
+manoj: do {
+
+System.out.print("*");
+
+m++;
+
+}while(m<-5);
+
+System.out.println();
+
+n++;
+break manu;
+
+}while(n<-5);
+
+}}
